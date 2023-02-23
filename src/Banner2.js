4b8fee9b397fe7519a29e8fda3edb384b840685c
jejovanni <jamaineekong@gmail.com>
2023-02-23 01:56:18 +0100

    still building landing

--- src/Banner2.js
+++ src/Banner2.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import "./Banner2.css"
 
 function Banner2() {
   return (
-    <div>
-        
+    <div id='Banner2'>
+      <div id='banner2left'>
+        <h1>Whether you're a local or a tourist</h1></div>
+      <div id='banner2middle'>
+        <img src="./Banner2Img.jpg" alt="" />
+      </div>
+      <div id='banner2right'>
+        <h1>find the latest and greatest happenings in town</h1>
+      </div>
     </div>
   )
 }
 
-export default Banner2
+export default Banner2;
---
+++ src/Banner3.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Banner3.css'
+
+function Banner3() {
+  return (
+    <div>Banner3</div>
+  )
+}
+
+export default Banner3
