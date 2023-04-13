4e161f426bcf33af16d45d7d02718dc7bbb64c33
jejovanni <jamaineekong@gmail.com>
2023-04-13 17:46:17 +0100

    new deploy

--- src/Banner1.js
+++ src/Banner1.js
@@ -11,7 +11,7 @@ function Banner1() {
         <Button className='btns' variant='contained'>I Want To Explore</Button>
       </div>
       <div id='Bannerright'>
-        <img src='./Banner1Img.jpg' />
+        <img src='./Banner1Img.jpg' alt=''/>
         {/* Photo by <a href="https://unsplash.com/@bluespit?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Shingi Rice</a> on <a href="https://unsplash.com/photos/kUmzO9mKLDk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */}
       </div>
     </div>
